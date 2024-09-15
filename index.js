45449023fc50cd388736f76037fe4cad1f518073
Mato Martinovic <martinovic.mato99@gmail.com>
2024-09-15 23:07:59 +0200

    add cors origin

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const app = express();
 app.disable("x-powered-by");
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://jelenavusurovic.me",
+  })
+);
 
 app.use("/_next", express.static(path.join(__dirname, ".next")));
 
