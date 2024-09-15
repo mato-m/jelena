ca1b94e1bb20d2304888634c3356784975fc08f4
Mato Martinovic <martinovic.mato99@gmail.com>
2024-09-15 23:15:03 +0200

    add cors origin

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import styles from "./Login.module.css";
 import Link from "next/link";
+import { toast } from "react-toastify";
 const Login = ({ setLoggedIn }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const handleLogin = async (event) => {
     event.preventDefault();
-    const response = await fetch("http://localhost:3001/api/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    });
-    if (response.ok) {
-      const data = await response.json();
-      if (data.auth === true) localStorage.setItem("jelenaJWT", data.token);
-      setLoggedIn(true);
+    try {
+      const response = await fetch("http://localhost:3001/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        if (data.auth === true) localStorage.setItem("jelenaJWT", data.token);
+        setLoggedIn(true);
+      } else throw new Error("Login failed");
+    } catch (error) {
+      alert(error);
+      toast("Greška pri prijavi");
     }
   };
   return (
