a70f7ba53d8288c58d9b2c1dc568bc80f51d5f91
Mato Martinovic <martinovic.mato99@gmail.com>
2024-09-16 02:09:04 +0200

    fix images

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,19 +7,13 @@ const Hero = ({ t }) => {
       <div className={styles.heroOverlay}>
         <Image
           quality={50}
-          data-aos="fade-up"
-          data-aos-duration="300"
           src="./navlogo.svg"
           alt="Jelena"
           width={300}
           height={300}
         />
-        <h1 data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
-          Jelena Vušurović
-        </h1>
-        <h2 data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
-          {t("mainSubtitle")}
-        </h2>
+        <h1>Jelena Vušurović</h1>
+        <h2>{t("mainSubtitle")}</h2>
       </div>
     </div>
   );
